bffcd8ec185c5971083c992cae4b2f5e30cb596b
Karol Boguniewicz <bogunk@Karols-MBP.frostlab.local>
2015-07-15 14:57:16 +0200

    Added stateful counter

--- hello.py
+++ hello.py
@@ -1,15 +1,18 @@
-import os
-import uuid
-from flask import Flask
-app = Flask(__name__)
-my_uuid = str(uuid.uuid1())
-BLUE = "#0099FF"
+import os			# import os-specific commands
+import uuid			# uuid module - generating unique ids
+from flask import Flask		# framework for quickly and easily building websites
+app = Flask(__name__)		# initialize web server
+my_uuid = str(uuid.uuid1())	# create new unique id each time
+BLUE = "#0000FF"		# const color code blue
 GREEN = "#33CC33"
+counter = 0
 
 COLOR = BLUE
 
-@app.route('/')
+@app.route('/')			# lines 11-23 - function; if webserver receives root, then handle that with this function
 def hello():
+	global counter
+	counter = counter+1
 
 	return """
 	<html>
@@ -17,11 +20,13 @@ def hello():
 
 	<center><h1><font color="white">Hi, I'm GUID:<br/>
 	{}
+	<br/>
+	Count: {}
 	</center>
 
 	</body>
 	</html>
-	""".format(COLOR,my_uuid)
+	""".format(COLOR,my_uuid,counter)
 
-if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
+if __name__ == "__main__":	# "pythinism" not required
+	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))	# run webserver, listen on every address on the port env def port or 5000 if cannot find it
